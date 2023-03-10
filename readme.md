67ac1e968aba80dac2033b516d49296f16eba64d
Raftouch <rafanelkin@gmail.com>
2023-03-10 21:25:22 +0100

    installed tailwind

---
+++ readme.md
@@ -0,0 +1,4 @@
+[x] install npm vite
+[x] create json with most used french words
+[x] install tailwind
+[ ]
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react"
+import words from "./wordList.json"
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState('Test')
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
   return (
-    <h1>Hi there</h1>
+    <div className="max-w-[800px] flex flex-col gap-2 m-auto">
+      <div className="text-center">
+        Lose
+        Win
+      </div>
+    </div>
   )
 }
 
